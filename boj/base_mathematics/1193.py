2b4dcb1a0f250512aab8abea579d1e0e70994162
BaeSungho <bacho4235@gmail.com>
2023-02-22 18:07:43 +0900

    [Add] 1193 python solution

---
+++ boj/base_mathematics/1193.py
@@ -0,0 +1,17 @@
+x = int(input())
+
+sum = 2
+index = 1
+row_index = 0
+
+while(1):
+    if x<=index:
+        row_index = index - x
+        if sum % 2 == 1:
+            print(str(sum-row_index-1) + '/' + str(row_index+1))
+            break
+        else:
+            print(str(row_index+1) + '/' + str(sum-row_index-1))
+            break
+    index += sum
+    sum += 1
